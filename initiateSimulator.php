0407c5b61a8059579c08ff7940964c74fedbec2e
Obinna Johnphill <obinna.johnphill@xbite.co.uk>
2021-09-06 14:32:52 +0100

    update code

--- initiateSimulator.php
+++ initiateSimulator.php
@@ -1,36 +1,30 @@
 #!/usr/bin/env php
-
 <?php
-
-
 require_once('vendor/autoload.php');
-
 
 use App\BattleSimulation\CombatantFactory;
 use App\BattleSimulation\Simulation;
 
+
+/**
+ * Initialisation of the application
+ */
+
 $factory   = new CombatantFactory();
 $describer = new Describer();
 
-// Get combatant names
 $firstName  = readline("What name will you deign to give the first combatant?\n");
 $secondName = readline("What name shall the second combatant receive?\n");
 
-// Create simulation
 $simulation = new Simulation($firstName, $secondName);
 
-// Describe combatants
 $describer->describeCombatants($simulation);
 
-// Start running turns
 while($turn = $simulation->performTurn()) {
-    echo "\n\nTurn " . $simulation->getTurnNumber() . ":\n\n";
-
+    print(PHP_EOL ." Turn " . $simulation->getTurnNumber() . ":".PHP_EOL);
     $describer->describeTurn($turn);
-
     if($simulation->isOver()) {
-        echo "\n";
-
+        print(PHP_EOL);
         if($simulation->isWon()) {
             $describer->describeBattleWon($simulation);
         } else {
