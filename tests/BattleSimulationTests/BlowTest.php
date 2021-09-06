ec757d81119b218afec42bcf18369a1631128204
Obinna Johnphill <obinna.johnphill@xbite.co.uk>
2021-09-06 17:46:20 +0100

    completed Blow Class unit tests

--- tests/BattleSimulationTests/BlowTest.php
+++ tests/BattleSimulationTests/BlowTest.php
@@ -1,11 +1,23 @@
-<?php 
+<?php
 
+use \App\BattleSimulation\Blow;
 use PHPUnit\Framework\TestCase;
 
 abstract class BlowTest extends TestCase {
-    function test2()
+
+    private $blow;
+
+    protected function setUp():void
     {
-        $this->assertTrue(true);
+        $this->blow = Blow::class(10, 5);
+    }
+
+    public function testClassInstance(){
+        $this->assertInstanceOf(Blow::class,  $this->blow);
+    }
+
+    public function testStrength(){
+        $this->assertIsInt($this->blow->getStrength());
     }
 }
 
